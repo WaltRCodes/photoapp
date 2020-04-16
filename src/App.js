a7962e4bd6a44a3711ce6222f1fc3bd30a11c27e
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-04-15 22:20:12 -0400

    made the constrctor

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import ApiDisplayPage from './components/ApiDisplayPage';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
 
   render (){
     return (
