046630137e3bad8caae5e8cb8959bd9c36c91f35
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-04-15 20:44:52 -0400

    added the render and return statement for the class

--- src/components/ApiDisplayPage.js
+++ src/components/ApiDisplayPage.js
@@ -1,2 +1,13 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+
+export default class ApiDisplayPage extends Component {
+
+    render (){
+        return (
+            <div >
+                ApiDisplayPage
+            </div>
+          );
+    }
+}
