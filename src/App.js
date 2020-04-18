d7b4c87cf66b1ecc25d02025b6b00308d5ac976d
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-04-18 14:02:37 -0400

    added state for who is logged in

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ export default class App extends Component {
     this.state = {
       loggedIn:false,
       apiData:[''],
-      contentPage:''
+      contentPage:'',
+      whoIsLoggedIn:''
     }
   }
 
